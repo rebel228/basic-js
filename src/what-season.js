b423c00db8316fd357bec21fc6caff8d97695e11
rebel228 <dinovik@rambler.ru>
2023-04-12 16:52:19 +0300

    feat: what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const deeperFakeDate = {
+  toString() {
+      return Date.prototype.toString.call(new Date());
+  },
+  getMonth() {
+      return Date.prototype.getMonth.call(new Date());
+  },
+  getFullYear() {
+      return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
+  },
+  getDate() {
+      return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
+  },
+  getHours() {
+      return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
+  },
+  getMinutes() {
+      return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
+  },
+  getSeconds() {
+      return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
+  },
+  getMilliseconds() {
+      return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
+  },
+  getDay() {
+      return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
+  },
+  [Symbol.toStringTag]: 'Date'
+};
+
+let asDate = new Date();
+
+Object.setPrototypeOf(deeperFakeDate, Object.getPrototypeOf(new Date()));
+
+function getSeason(date) {
+  console.log(date);
+  try {
+    console.log(date.getTime(date));
+  }
+  catch (error) {
+    throw new NotImplementedError('Invalid date!');
+  }
+  if(!date) return 'Unable to determine the time of year!';
+  if (isNaN(Date.parse(date))) throw new NotImplementedError('Invalid date!');
+  let month = date.getMonth(date);
+  let season = '';
+  console.log(month);
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      season = 'winter';
+      break;
+    case 2:
+    case 3:
+    case 4:
+      season = 'spring';
+      break;
+    case 5:
+    case 6:
+    case 7:
+      season = 'summer';
+      break;
+    case 8:
+    case 9:
+    case 10:
+      season = "autumn"
+      break;
+  }
+  console.log(season);
+  return season;
 }
+
 
 module.exports = {
   getSeason
