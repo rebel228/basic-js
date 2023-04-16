f137fff492af03e15882020852f4d90d634e8e3a
rebel228 <dinovik@rambler.ru>
2023-04-16 16:51:48 +0300

    feat: dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+let domains = ['code.yandex.ru','music.yandex.ru','yandex.ru'];
+
+function getDNSStats(domains) {
+  result = {};
+  for (let i = 0; i < domains.length; i ++) {
+    let str = '';
+    for (let j = domains[i].length - 1; j >= 0; j--) {
+      if (domains[i][j]  === '.') {
+        str = domains[i].slice(j);
+        str = '.' + str.split('.').reverse().join('.').slice(0, str.length - 1);
+        if(str in result) {
+          result[str] = result[str] + 1;
+        }
+        else result[str] = 1;
+      }
+      if (j === 0) {
+        str = domains[i].slice(j);
+        str = '.' + str.split('.').reverse().join('.').slice(0, str.length);
+        if(str in result) {
+          result[str] = result[str] + 1;
+        }
+        else result[str] = 1;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
